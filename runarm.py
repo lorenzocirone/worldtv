4e83ec4c8147ab341643e5d6bfc2de1dee4b9c4b
Lorenzo Cirone <109392566+lorenzocirone@users.noreply.github.com>
2026-08-29 19:20:31 +0200

    Delete runarm.py

--- runarm.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Bootstrap per livetv.so (codice nativo compilato)."""
-import importlib.util, sys
-spec = importlib.util.spec_from_file_location("livetv_main", "livetvarm.so")
-mod = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(mod)
-mod.main()
